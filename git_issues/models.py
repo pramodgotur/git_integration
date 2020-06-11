775f99b095b0f5172852c74a4cb2306237882132
pramod gotur <pramodgotur1995@gmail.com>
2020-06-11 14:08:09 +0530

    solve error from models

--- git_issues/models.py
+++ git_issues/models.py
@@ -15,8 +15,8 @@ class GithubIssues(models.Model):
     title = models.CharField(max_length=500)
     issue_number = models.IntegerField(db_index=True)
     assignees = models.ManyToManyField(
-        GithubUsers, on_delete=models.CASCADE, related_name="issue_assignees")
+        GithubUsers, related_name="issue_assignees")
     lables = models.ManyToManyField(
-        GithubUsers, on_delete=models.CASCADE, related_name="issue_lables")
-    issue_created_at = models.DatetimeField()
+        GithubUsers, related_name="issue_lables")
+    issue_created_at = models.DateTimeField()
     issue_url = models.CharField(max_length=200)
